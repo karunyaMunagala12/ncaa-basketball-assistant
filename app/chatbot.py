bcd7c484c8c1bbeff9f824b83f86f448ebb19f52
Karunya Munagala <135272634+karunyaMunagala12@users.noreply.github.com>
2025-07-20 10:42:34 -0500

    Update chatbot.py

--- app/chatbot.py
+++ app/chatbot.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import pandas as pd
 import json
 from sentence_transformers import SentenceTransformer
-from pinecone import Pinecone
+import pinecone
 from groq import Groq
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics.pairwise import cosine_similarity
@@ -19,7 +19,9 @@ PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 # === INIT Services ===
-pc = Pinecone(api_key=PINECONE_API_KEY)
+pinecone.init(api_key=PINECONE_API_KEY)
+index = pinecone.Index("team-scouting")
+
 groq_client = Groq(api_key=GROQ_API_KEY)
 embed_model = SentenceTransformer("all-MiniLM-L6-v2")
 
